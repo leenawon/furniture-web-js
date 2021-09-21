e2efcbc6ba05e6ce24722228513967341ad07aeb
leenawon <nawon5154@gmail.com>
2021-09-22 02:51:32 +0900

    Cart Button event listener 추가 작성

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,13 @@
 const navbarToggleButton = document.querySelector('.navbar-toggle-button');
 const navbarMenu = document.querySelector('.navbar-menu');
+const cartButton = document.querySelector('.cart-button');
+const cartWrapper = document.querySelector('.cart-wrapper');
+const productList = document.querySelector('.product-list');
+
+// Handle Cart Button
+function handleCartButton() {
+  cartWrapper.classList.toggle('active-cart-wrapper');
+}
 
 // Handle Toggle Button
 function handleToggleButton() {
@@ -9,6 +17,8 @@ function handleToggleButton() {
 function allEventListeners() {
   // Toggle navbar button
   navbarToggleButton.addEventListener('click', handleToggleButton);
+  // Cart Button
+  cartButton.addEventListener('click', handleCartButton);
 }
 
 allEventListeners();
