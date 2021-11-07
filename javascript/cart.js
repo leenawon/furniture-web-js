00ec750f7246c4160afd5417d9c5e83eb0b635fb
leenawon <nawon5154@gmail.com>
2021-11-07 20:47:37 +0900

    target 상품의 정보 얻는 함수 추가(getProductInfo)

--- javascript/cart.js
+++ javascript/cart.js
@@ -4,6 +4,18 @@ const totalCartPrice = document.querySelector('.total-cart-price');
 
 let CART_ITEM_ID = 1;
 
+// Get Product Information 
+function getProductInfo(productItem) {
+  let productInfo = {
+    id: CART_ITEM_ID,
+    image: productItem.querySelector('.product-image img').src,
+    name: productItem.querySelector('.product-description h3').textContent,
+    category: productItem.querySelector('.product-category').textContent,
+    price: productItem.querySelector('.product-price').textContent
+  }
+  CART_ITEM_ID++;
+}
+
 // Buying Product (add Product Button)
 function buyingProduct(e) {
   if(e.target.classList.contains('add-product-button')) {
